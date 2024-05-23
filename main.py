dacc012680f6ee175afffd4af69778fdb54fe52e
Curt Varron <aprilcathlie@gmail.com>
2024-05-23 13:58:58 +0800

    def VOLUME, CHANNEL (??)

--- main.py
+++ main.py
@@ -26,3 +26,17 @@ class TV:
         if 1 <= channel <= 120:
             self.channel = channel
 
+    def getVolume(self):
+        return self.volumeLevel
+
+    def setVolume(self, volumeLevel):
+        if 1 <= volumeLevel <= 7:
+            self.volumeLevel = volumeLevel
+
+    def channelUp(self):
+        if self.channel < 120:
+            self.channel += 1
+
+    def channelDown(self):
+        if self.channel > 1:
+            self.channel -= 1
